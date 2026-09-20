2e10144a8e008534dc77ec09c2042c151dea035f
olesya <bulchuk.olesya@gmail.com>
2026-09-20 18:49:33 +0700

    feat: расширение для внедрения репозиториев

---
+++ Store/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Store.Repositories;
+
+namespace Store;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddHomeLibraryStore(
+        this IServiceCollection services)
+    {
+        services.AddTransient<IDbConnectionFactory, DbConnectionFactory>();
+
+        services.AddTransient<IPublisherRepository, PublisherRepository>();
+        services.AddTransient<IAuthorRepository, AuthorRepository>();
+        services.AddTransient<IGenreRepository, GenreRepository>();
+        services.AddTransient<IBookRepository, BookRepository>();
+
+        return services;
+    }
+}
